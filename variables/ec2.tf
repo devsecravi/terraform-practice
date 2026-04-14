9701029e3de4c6d08713e0ea5e55d9d436a04561
unknown <dasariravid145@gmail.com>
2026-04-14 10:49:35 +0530

    action

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
 
-  ami           = var.ami_id # Amazon Linux 2 (official free-tier compatible)
-  instance_type = var.instance_type
+  ami           = var.ami_id 
+  instance_type = var.environment=="dev" ? "t3.micro" : "t3.small"
 
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
