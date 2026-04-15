9d006217080ac3e9fe6571d75d2f5fd00ded3290
unknown <dasariravid145@gmail.com>
2026-04-15 21:41:10 +0530

    action

--- roboshop/variables.tf
+++ roboshop/variables.tf
@@ -4,3 +4,10 @@ variable  "tags_list"{
      default = ["mongodb", "redis", "mysql", "rabbitmq", "catalogue", "user", "cart", "shipping", "payment", "frontend"]
 }
 
+variable "zone_id" {
+    default = "Z05013202FKF0ZL12WAOP"
+}
+
+variable "domain_name" {
+    default = "daws88s.online"
+}
