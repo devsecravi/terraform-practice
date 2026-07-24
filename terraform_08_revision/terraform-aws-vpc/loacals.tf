dd5ee68f5e36872a7d25fd8fb361f1e4f759520a
unknown <dasariravid145@gmail.com>
2026-07-24 23:36:18 +0530

    terraform

--- terraform_08_revision/terraform-aws-vpc/loacals.tf
+++ terraform_08_revision/terraform-aws-vpc/loacals.tf
@@ -14,5 +14,15 @@ locals {
              Name = "${var.project}-${var.environment}"
         },
         var.vpc_tags
-  )
+    )
+    gw_final_tags= merge(
+           local.common_tags,
+           {
+                Name = "${var.project}-${var.environment}"
+           },
+           var.gw_tags
+    )
+    az_names = slice(data.aws_availability_zones.available.names, 0, 2)
+
+    
 }
